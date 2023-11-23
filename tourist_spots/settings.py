e5503a408c3f0d6e276219c211e19a373df09cc6
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-11-23 14:20:17 -0300

    feat: Add configs to lib django-filters

--- tourist_spots/settings.py
+++ tourist_spots/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'core',
     'attractions',
     'comments',
@@ -130,3 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = 'images'
 MEDIA_URL = '/media/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+}
