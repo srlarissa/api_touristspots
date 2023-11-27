6680b78db6064846caf81452fd70569b306811a1
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-11-27 14:39:56 -0300

    feat: Total integration auth

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -2,11 +2,15 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter
 from core.models import TouristSpot
 from .serializers import TouristSpotSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 class TouristSpotViewSet(ModelViewSet):
     serializer_class = TouristSpotSerializer
     filter_backends=[SearchFilter]
     search_fields=['name', 'description']
+    permission_classes=[IsAuthenticated]
+    authentication_classes=[TokenAuthentication]
 
     def get_queryset(self):
        id = self.request.query_params.get('id', None)
