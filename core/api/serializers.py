380cafd8e2c24aee48251ae055c0c72a8aebd397
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-11-27 15:36:53 -0300

    feat: Add pagination

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework.serializers import ModelSerializer
 from core.models import TouristSpot
+from attractions.api.serializers import AttractionSerializer
+from localizations.api.serializers import LocalizationSerializer
+from comments.api.serializers import CommentSerializer
+from ratings.api.serializers import RatingSerializer
 
 class TouristSpotSerializer(ModelSerializer):
+    attractions = AttractionSerializer(many=True, read_only=True)
+    localization = LocalizationSerializer(read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
+    ratings = RatingSerializer(many=True, read_only=True)
     class Meta:
         model = TouristSpot
-        fields = ['id', 'name', 'description', 'photo']
+        fields = ['id', 'name', 'description', 'photo', 'attractions', 'localization', 'comments', 'ratings']
